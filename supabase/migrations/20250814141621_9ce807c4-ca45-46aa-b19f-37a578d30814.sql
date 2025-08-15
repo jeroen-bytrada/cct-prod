0198caa031b080b564cd28f4861acfe1566df455
Buzzaroo <jeroen@bytrada.com>
2025-08-15 14:21:01 +0200

    Delete supabase/migrations/20250814141621_9ce807c4-ca45-46aa-b19f-37a578d30814.sql

--- supabase/migrations/20250814141621_9ce807c4-ca45-46aa-b19f-37a578d30814.sql
+++
@@ -1,6 +0,0 @@
--- Remove the security definer views that the linter flagged
-DROP VIEW IF EXISTS public.cct_customers;
-DROP VIEW IF EXISTS public.cct_stats;
-
--- The security definer functions (get_cct_customers and get_cct_stats) will remain 
--- but the application code will need to call them directly instead of through views
